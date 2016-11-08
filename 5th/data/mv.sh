dda3b123ada12b9b1674c1dc1ab58c5b64c6f6f1
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-11-08 20:00:45 +0900

    delete shell script

--- 5th/data/mv.sh
+++
@@ -1,9 +0,0 @@
-for name in sangenya itano nagura nakagawa
-do
-    cd $name
-        for filename in $(ls)
-        do
-            mv $filename ./${filename/${name}_}
-        done
-    cd ..
-done
